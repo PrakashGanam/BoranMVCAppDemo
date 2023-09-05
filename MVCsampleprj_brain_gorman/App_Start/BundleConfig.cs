76124a126ccfded247dd2a2efb3404c984e2cfd8
Prakash S <prakash181992@outlook.com>
2023-09-05 10:55:45 +0530

    Bundle config

--- MVCsampleprj_brain_gorman/App_Start/BundleConfig.cs
+++ MVCsampleprj_brain_gorman/App_Start/BundleConfig.cs
@@ -6,6 +6,7 @@ namespace MVCsampleprj_brain_gorman
     public class BundleConfig
     {
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
