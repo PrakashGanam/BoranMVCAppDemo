3559fb79ad29b84f01432b31d8ff9b046f947173
Prakash S <prakash181992@outlook.com>
2023-09-06 16:05:00 +0530

    Contacct, State details added

---
+++ MVCsampleprj_brain_gorman/Models/ContactwebConstants.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCsampleprj_brain_gorman.Models
+{
+    public class ContactwebConstants
+    {
+        public const int MAX_STATE_NAME_LENGTH = 50;
+
+        public const int MAX_STATE_ABBR_LENGTH = 4;
+
+        public const int MAX_FIRST_NAME_LENGTH = 50;
+        public const int MAX_LAST_NAME_LENGTH = 50;
+        public const int MAX_EMAIL_LENGTH = 250;
+        public const int MAX_PHONE_LENGTH = 15;
+        public const int MAX_STREET_ADDRESS_LENGTH = 150;
+        public const int MAX_CITY_LENGTH = 25;
+        public const int MAX_ZIPCODE_LENGTH = 10;
+        public const int MIN_ZIPCODE_LENGTH = 5;
+    }
+}
